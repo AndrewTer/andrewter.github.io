1bbcca02263cebe94287f19ec9b682a5f4c8b010
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2024-05-31 11:40:25 +0300

    Add files via upload

---
+++ assets/js/projects.js
@@ -0,0 +1,12 @@
+// Fancybox Configuration
+$('[data-fancybox="gallery"]').fancybox({
+  buttons: [
+    "slideShow",
+    "thumbs",
+    "zoom",
+    "fullScreen",
+    "close"
+  ],
+  loop: false,
+  protect: true
+});
